d3bc2b82203d319b88846a74d932612887d14415
yuzukicat <yuzuki.cat@kamisu66.com>
2023-04-17 17:44:02 +0900

    customize improve footer

--- packages/components/src/components/footer-extended.tsx
+++ packages/components/src/components/footer-extended.tsx
@@ -8,9 +8,9 @@ export const FooterExtended = ({
   return (
     <footer className={clsx('bg-white text-xs dark:bg-[#111]', className)}>
       <div className="container max-w-[90rem] border-t border-gray-300 dark:border-gray-800">
-        <div className="my-8 flex flex-col gap-6 pt-2 pb-4 lg:flex-row">
+        <div className="my-8 flex flex-col gap-6 pt-2 pb-4 lg:flex-row items-center sm:items-start">
           <div className="flex w-full flex-row gap-6">
-          <p className="text-pink-500 dark:text-gray-200">@2023 Yuzuki.Cat</p>
+          <p className="text-pink-500 dark:text-gray-200 mt-6 text-xs">© {new Date().getFullYear()} Yuzuki.Cat</p>
           </div>
         </div>
       </div>
